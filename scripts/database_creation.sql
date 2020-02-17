7474c104c90f464ead73f50bd0b3b357d255c977
Hair Albeiro Parra Barrera <jair.parra@outlook.com>
2020-02-17 15:43:51 -0500

    Finihshed v1 of tablecreation

--- scripts/database_creation.sql
+++ scripts/database_creation.sql
@@ -10,6 +10,12 @@ DROP TABLE IF EXISTS usertable CASCADE;
 DROP TABLE IF EXISTS request ; 
 DROP TABLE IF EXISTS invoice CASCADE; 
 DROP TABLE IF EXISTS orderTable CASCADE; 
+DROP TABLE IF EXISTS activity CASCADE;
+DROP TABLE IF EXISTS startTable CASCADE;
+DROP TABLE IF EXISTS modify CASCADE;
+DROP TABLE IF EXISTS generate CASCADE;
+
+
 
 -- NOTE: Cannot use keyword 'user'
 CREATE TABLE usertable
@@ -134,4 +140,58 @@ ALTER TABLE invoice
   ADD FOREIGN KEY (oid) REFERENCES orderTable (oid) ; 
 
 
-END
+--- Neijin's updates
+
+CREATE TABLE activity
+(
+    aid SERIAL NOT NULL,
+    oid INTEGER NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    mngName VARCHAR(50) NOT NULL,
+    PRIMARY KEY (aid),
+    FOREIGN KEY (oid) REFERENCES orderTable(oid)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (mngName) REFERENCES manager(username)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- Note1 :Use starttable not start since start is a keyword
+-- Note2 : Add an attribute called startDate
+CREATE TABLE startTable
+(
+    rid INTEGER NOT NULL,
+    mateName VARCHAR(50) NOT NULL,
+    custName VARCHAR(50) NOT NULL,
+    startDate DATE NOT NULL,  --start date
+    startTime TIME NOT NULL,
+    PRIMARY KEY (mateName, custName) ,
+    FOREIGN KEY (mateName) REFERENCES mate(username),
+    FOREIGN KEY (custName) REFERENCES customer(username),
+    FOREIGN KEY (rid) REFERENCES request(rid)
+);
+
+CREATE TABLE modify
+(
+    mngName VARCHAR(50) NOT NULL,
+    oid INTEGER NOT NULL,
+    modTime TIME NOT NULL,
+    modDate DATE NOT NULL,
+    
+    FOREIGN KEY (oid) REFERENCES orderTable(oid),
+    FOREIGN KEY (mngName) REFERENCES manager(username),
+    PRIMARY KEY (mngName,oid)
+);
+
+CREATE TABLE generate
+(
+    rid INTEGER NOT NULL,
+    oid INTEGER NOT NULL,
+
+    PRIMARY KEY (rid), 
+    FOREIGN KEY (rid )REFERENCES request(rid)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (oid) REFERENCES orderTable(oid)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+END.
