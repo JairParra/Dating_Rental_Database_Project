cb6175195a9c11a90aeaa67f8bc4ea3437188e08
Hair Albeiro Parra Barrera <jair.parra@outlook.com>
2020-02-27 18:27:09 -0500

    Updated ordertable

--- scripts/tables_insertions/7_order_insertions.sql
+++ scripts/tables_insertions/7_order_insertions.sql
@@ -1,14 +1,14 @@
 INSERT INTO orderTable (oid, startDate,endDate, ordStatus,rid) 
-                VALUES(1,'2018-09-13','2019-03-19','active','1');
-INSERT INTO orderTable VALUES (2,'2018-11-19','2019-06-11','pending',2,'2019-06-21','comments2',3.9);
-INSERT INTO orderTable VALUES (3,'2019-11-20','2020-02-08','pending',3,'2020-02-13','comments3',4.0);
+                VALUES(1,'2018-05-16','2019-11-17','complete','6');
+INSERT INTO orderTable VALUES (2,'2018-03-09','2020-01-16','pending',8,'2020-02-02','comments2',3.0);
+INSERT INTO orderTable VALUES (3,'2019-10-01','2020-02-19','complete',5,'2020-02-19','comments3',0.6);
 INSERT INTO orderTable (oid, startDate,endDate, ordStatus,rid) 
-                VALUES(4,'2019-11-04','2019-11-16','complete','4');
-INSERT INTO orderTable VALUES (5,'2019-09-29','2020-01-09','pending',5,'2020-02-16','comments5',0.8);
-INSERT INTO orderTable VALUES (6,'2018-02-23','2019-05-19','complete',6,'2019-11-05','comments6',0.5);
+                VALUES(4,'2020-02-22','2020-02-23','complete','7');
+INSERT INTO orderTable VALUES (5,'2018-05-14','2019-12-18','active',10,'2019-12-21','comments5',3.5);
+INSERT INTO orderTable VALUES (6,'2020-02-05','2020-02-05','active',9,'2020-02-14','comments6',2.6);
 INSERT INTO orderTable (oid, startDate,endDate, ordStatus,rid) 
-                VALUES(7,'2019-08-27','2020-02-23','active','7');
-INSERT INTO orderTable VALUES (8,'2019-08-12','2019-10-14','active',8,'2019-12-06','comments8',1.0);
-INSERT INTO orderTable VALUES (9,'2020-01-08','2020-02-12','pending',9,'2020-02-19','comments9',1.7);
+                VALUES(7,'2018-02-17','2020-02-14','complete','4');
+INSERT INTO orderTable VALUES (8,'2019-07-16','2019-08-23','complete',2,'2019-09-18','comments8',3.7);
+INSERT INTO orderTable VALUES (9,'2018-08-24','2018-10-08','active',3,'2019-08-13','comments9',1.8);
 INSERT INTO orderTable (oid, startDate,endDate, ordStatus,rid) 
-                VALUES(10,'2019-02-23','2019-03-16','active','10');
+                VALUES(10,'2020-02-23','2020-02-25','active','1');
