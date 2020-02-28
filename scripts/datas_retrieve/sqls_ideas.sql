bfb7e3145d7a8c9f8b001651a68b63d307d92018
chellycheng <hehuimin.cheng@mail.mcgill.ca>
2020-02-27 21:41:34 -0500

    beautifulsyntax

--- scripts/datas_retrieve/sqls_ideas.sql
+++ scripts/datas_retrieve/sqls_ideas.sql
@@ -20,9 +20,24 @@ SELECT matename FROM mateRating2 WHERE Average > 4.0
 -- Background: Say system has a bug, the orders has some problem between a certain time have problems, the manager want to find the cust, mate id to contact and solve the problem for the order)
 -- Find all manager, cust, mate usename of order between some time interval
 
-CREATE VIEW timeRequest (oid,rid) AS
-SELECT oid,rid FROM orderTable WHERE ((DATE_PART('year', '2018-02-02'::date) - DATE_PART('year',enddate::date)) * 12 + (DATE_PART('month', '2018-02-02'::date) - DATE_PART('month', enddate::date))) <= 100
+CREATE VIEW timeRequest 
+    (oid,rid) AS
+SELECT 
+    oid,
+    rid 
+FROM orderTable 
+WHERE ((DATE_PART('year', '2018-02-02'::date) - DATE_PART('year',enddate::date)) * 12 + (DATE_PART('month', '2018-02-02'::date) - DATE_PART('month', enddate::date))) <= 10
 ;
 
-SELECT custname,matename,mngname FROM timeRequest as temp, request,modification WHERE temp.rid=request.rid AND temp.oid=modification.oid
+SELECT 
+    custname, 
+    matename,
+    mngname
+FROM timeRequest temp
+INNER JOIN request r
+    ON temp.rid = r.rid 
+JOIN modification mod 
+    ON mod.oid = temp.oid 
+; 
+
 
--- scripts/datas_retrieve/test.sql
+++ scripts/datas_retrieve/test.sql
@@ -1,7 +1,10 @@
---
-
-CREATE VIEW timeRequest (oid,rid) AS
-SELECT oid,rid FROM orderTable WHERE ((DATE_PART('year', '2018-02-02'::date) - DATE_PART('year',enddate::date)) * 12 + (DATE_PART('month', '2018-02-02'::date) - DATE_PART('month', enddate::date))) <= 100
-;
-
-SELECT custname,matename,mngname FROM timeRequest as temp, request,modification WHERE temp.rid=request.rid AND temp.oid=modification.oid
+SELECT 
+    custname, 
+    matename,
+    mngname
+FROM timeRequest temp
+INNER JOIN request r
+    ON temp.rid = r.rid 
+JOIN modification mod 
+    ON mod.oid = temp.oid 
+; 
