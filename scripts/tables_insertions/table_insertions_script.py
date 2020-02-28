ea4b11a0aba2cae9667ab2d94f922f1f7a7179c0
qqqqyyy <44212024+qqqqyyy@users.noreply.github.com>
2020-02-28 07:36:43 -0500

    Add files via upload

--- scripts/tables_insertions/table_insertions_script.py
+++ scripts/tables_insertions/table_insertions_script.py
@@ -27,23 +27,23 @@ random.seed(42)
 usertable = pd.read_csv('../../data_raw/user_table.csv', encoding='utf8')  # read artificial user table
 userdict = usertable.set_index('username').to_dict() # convert to dictionary for easy search 
 
-## Managers 
+## list of Managers 
 usernames = list(usertable['username']) # select all usernames 
-managers = list(np.random.choice(usernames, size=5, replace=False)) # select 10 managers 
+managers = list(np.random.choice(usernames, size=5, replace=False)) # select 5 random managers 
 
-## Mates 
+## list of Mates 
 usernames2 = [name for name in usernames if name not in managers] # substract mates
 possible_male_mates = [username for username in usernames2 if 
                        userdict['dateofbirth'][username] < '2000-01-01' and 
                        userdict['sex'][username] == 'Male']
 possible_female_mates = [username for username in usernames2 if 
                        userdict['dateofbirth'][username] < '2000-01-01' and 
-                       userdict['sex'][username] == 'Female']
+                       userdict['sex'][username] == 'Female']  # exclude users who's under 18 //suppose the business started in 2018
 male_mates = list(np.random.choice(possible_male_mates,size=5,replace=False)) 
-female_mates = list(np.random.choice(possible_female_mates,size=5,replace=False)) 
-mates = male_mates + female_mates 
+female_mates = list(np.random.choice(possible_female_mates,size=5,replace=False)) # randomly choose 5 male and 5 female from possible list
+mates = male_mates + female_mates #as mate
 
-## Customers 
+## list of Customers 
 customers = [username for username in usernames2 if username not in mates] 
 
 
@@ -55,7 +55,7 @@ customers = [username for username in usernames2 if username not in mates]
 
 ## 3.1 create_mate
 
-def create_mate(mates): 
+def create_mate(mates): # argument is a list of mate
     
     records = [] # list of strings 
     
@@ -68,17 +68,18 @@ def create_mate(mates):
         height = round(random.uniform(1.50, 2.00),2) # generate a random height 
         weight = round(random.uniform(55.00, 75.00))  # generate a random weight 
         hourlyRate = random.randint(50,200) # generate a random hourly rate 
-        language = np.random.choice(languages, replace=False)
+        language = np.random.choice(languages, replace=False) # randomly choose one language spoken; modify in language list if needs more than one 
         
         stmt = "INSERT INTO mate VALUES('{}','{}','{}','{}',{},{},{}); \n".format(username, 
                                        nicknames[i], descriptions[i], language, height, weight, hourlyRate)
-        
-        records += [stmt]
+        # SQL: INSERT INTO mate VALUES('username', 'nickname', 'description', language, height, weight, hR)
+
+        records += [stmt] # save statements to list
         i += 1 
         
     return records
         
-# create the mates statements 
+# create the list of mates statements 
 mates_insertion = create_mate(mates)
 
 #save the table 
@@ -87,11 +88,10 @@ with open("2_mate_insertions.sql", "w") as file:
     file.close() 
     
 #####################################################x##########################
-
     
 ## 3.2  customer_table 
     
-def create_customer(customers): 
+def create_customer(customers): # argument is a list of customer
     
     records = []  # store records
     
@@ -100,7 +100,9 @@ def create_customer(customers):
     
     for username in customers: 
         stmt = "INSERT INTO customer VALUES('{}','{}'); \n".format(username, preferences[i]) 
-        records += [stmt]
+         # SQL: INSERT INTO customer VALUES('username', 'preference')
+
+        records += [stmt] # save statements to list
         i += 1 
         
     return records 
@@ -113,24 +115,24 @@ with open("3_customer_insertions.sql", "w") as file:
     file.writelines(customer_insertion) 
     file.close() 
     
-    
 ###############################################################################
-
     
 ## 3.3 manager table 
     
-def create_manager(managers): 
+def create_manager(managers): # argument is a list of manager
     
     records = [] # store the records 
     
     # create SQL INSERT statements
     for username in managers: 
         stmt = "INSERT INTO manager VALUES('{}'); \n".format(username) 
-        records += [stmt] 
+        # SQL: INSERT INTO manager VALUES('username')
+
+        records += [stmt]  # save statements to list
     
     return records
 
-# create the mates statements 
+# create the manager statements 
 manager_insertion = create_manager(managers)
 
  #save the table 
@@ -138,27 +140,16 @@ with open("4_manager_insertions.sql", "w") as file:
     file.writelines(manager_insertion) 
     file.close()  
     
-    
 ###############################################################################
-
 
 ## 3.4 application table 
     
-def create_application(mates, managers): 
-    """ 
-    Will create insertion statements for the application table of the form: 
-        INSERT INTO application VALUES(appid, username, aTime, isApproved, mngName)
-    respecting the appropriate constraints. 
-    @ args: 
-        @ mates: a list of mate usernames 
-        @ managers: a list of manager usernames
-    """
+def create_application(mates, managers): # arguments are a list of mate and a list of manager
     
     # Note: Number of mates has to be at least the number of managers
     if len(mates) < len(managers): 
         raise ValueError("Cannot have more managers than mates")
         
-    
     records = []  # store the records 
 
     # Choose manager names with replacement
@@ -181,8 +172,9 @@ def create_application(mates, managers):
         
         stmt = "INSERT INTO application VALUES({},'{}','{}','{}','{}');\n".format(
                         i+1,mateName, mngName, aTime, appStatus) 
+        # SQL: INSERT INTO application VALUES(appid, username, aTime, isApproved, mngName)
         
-        records += [stmt] 
+        records += [stmt]  # save statements to list
         
     return records 
 
@@ -198,34 +190,21 @@ with open("5_application_insertions.sql", "w") as file:
 
 ### 3.5 request table 
     
-def create_request(mates, customers, size=10): 
-    """ 
-    Will create insertion statements for the application table of the form: 
-        INSERT INTO application (rid, rinfo, rstatus, custName, mateName, decTime) VALUES( -,-,-,-,-,- )
-    respecting the appropriate constraints. 
-    @ args: 
-        @ mates: a list of mate usernames 
-        @ managers: a list of customers
-        
-    NOTE: A request must have exactly one Mate and exactly one Customer, but 
-        a customer can book several Mates, and a single Mate can be booked by several 
-        customers (see ER diagram). Request decDate CAN overlap! 
-    """
+def create_request(mates, customers, size=10): #arguments are a list of mate, a list of customer, and size as integer
     
     records = []  # store the records 
 
     # Choose mate and customer names without replacement
     mateNames = np.random.choice(mates, size=size, replace=True) 
     customerNames = np.random.choice(customers, size=size, replace=True)
-    
-    
+       
     # Set up time generation objects 
     fake_time = Faker()
     start_date = datetime.date(year=2018, month=1,day=1) # suppose our business started in 2018
     request_dates = [fake_time.date_between(start_date=start_date, end_date='today') for i in range(size)] 
-    decision_dates = [fake_time.date_between(start_date=request_dates[i], end_date='today') for i in range(size)]
-    request_dates = [str(date) for date in request_dates]  # convert back to string format
-    decision_dates= [str(date) for date in decision_dates]  # convert back to string format ?
+    decision_dates = [fake_time.date_between(start_date=request_dates[i], end_date='today') for i in range(size)] # randomly choose set of times
+    request_dates = [str(date) for date in request_dates] 
+    decision_dates= [str(date) for date in decision_dates]  # convert to string format
     
     # statuses 
     statuses = ["pending","rejected","accepted"]
@@ -238,9 +217,9 @@ def create_request(mates, customers, size=10):
     # create size number of records
     for i in range(size): 
         
-        rid = i+1
+        rid = i+1 # rid starts from 1
         rstatus = np.random.choice(statuses, p=[0.30,0.35,0.35]) # choose status randomly 
-        rinfo = "Information" + str(random.randint(1,100))
+        rinfo = "Information" + str(random.randint(1,100))  # format
         custName = customerNames[i] 
         mateName = mateNames[i] 
         rdate = request_dates[i] 
@@ -250,17 +229,17 @@ def create_request(mates, customers, size=10):
         if i % 6 != 0: 
             stmt = "INSERT INTO request VALUES({},'{}','{}','{}','{}','{}','{}');\n".format(
                 rid,rinfo,rstatus,custName,mateName,rdate,decDate)  
+            #SQL: INSERT INTO request VALUES(rid, rinfo, rstatus, custName, mateName, decTime)
         else: 
             stmt = "INSERT INTO request VALUES({},'{}',DEFAULT,'{}','{}','{}','{}');\n".format(
                 rid,rinfo,custName,mateName,rdate,decDate)              
-        
+            #SQL: INSERT INTO request VALUES(rid, rinfo, DEFAULT, custName, mateName, decTime)
         rids += [rid]
         custnames += [custName]
         matenames += [mateName]
-        records += [stmt]  
+        records += [stmt]   # save statements to list
         
     return rids, custnames, matenames, records 
-
 
 # create the request statements 
 rids ,rcustnames, rmatenames, request_insertion = create_request(mates, customers) # note that we have also the rids produced
@@ -270,17 +249,11 @@ with open("6_request_insertions.sql", "w") as file:
     file.writelines(request_insertion) 
     file.close()  
     
-
 ###############################################################################
 
-### 3.7 order table 
+### 3.6 order table 
     
-def create_order(rids):
-    """
-    Generate insertions of the shape: 
-        INSERT INTO orderTable VALUES (oid, startDate, endDate, ordStatus, rid, ratingDate, comment, rating)
-    NOTE: We want the rids to be UNIQUE!!! >> For each rid, we want to create an order.
-    """
+def create_order(rids): #argument is a list of request id
 
     rids = list(np.random.permutation(rids)) # permute randomly
     size = len(rids)
@@ -315,17 +288,19 @@ def create_order(rids):
         if( i % 3 != 0 ):
             stmt = "INSERT INTO orderTable VALUES ({},'{}','{}','{}',{},'{}','{}',{});\n".format(
                 oid, str(start_date), end_date, ordstatus, rid, rate_date, comment, rating)
+            #SQL: INSERT INTO orderTable VALUES (oid, startDate, endDate, ordStatus, rid, ratingDate, comment, rating)
         else:
             stmt = """INSERT INTO orderTable (oid, startDate,endDate, ordStatus,rid) 
                 VALUES({},'{}','{}','{}','{}');\n""".format(
                 oid, str(start_date), end_date, ordstatus,  rid)
+            #SQL:  INSERT INTO orderTable (oid, startDate, endDate, ordStatus, rid) VALUES (-,-,-,-,-)
+            # represent requests without rating
             
         # update return values
-        records += [stmt]
+        records += [stmt] # save statements to list
         oids += [oid]
 
     return oids, records
-
 
 # create the request statements
 oids, order_insertion = create_order(rids)
@@ -335,23 +310,11 @@ with open("7_order_insertions.sql", "w") as file:
     file.writelines(order_insertion)
     file.close()
 
-
 ###############################################################################
 
-### 3.8 invoice table 
+### 3.7 invoice table 
     
-## ** WRITE def create_<table>(): here ** ### 
-def create_invoice(customers, oids):
-    """
-    Will create insertion statements for the invoice table of the form:
-    inid, oid, description, dueDate, amount, custName , method, status  (pending, paid)
-        INSERT INTO invoice (inid, oid, description, dueDate, amount,custName , method, status)  VALUES( -,-,-,-,-,- )
-    respecting the appropriate constraints.
-    @ args:
-        @ customers: a list of customers
-        
-    NOTE: order ids are UNIQUE, but can be permuted.
-    """
+def create_invoice(customers, oids): # arguments are a list of customer and a list of order id
 
     size = len(oids)
     oids = list(np.random.permutation(oids))
@@ -386,12 +349,12 @@ def create_invoice(customers, oids):
         amount = round(random.uniform(15.00, 200.00))
         stmt = "INSERT INTO invoice VALUES({},{},'{}','{}','{}','{}','{}','{}');\n".format(
             inid, oid, descriptions[i], due_date, amount, custName, method, status)
+        #SQL: INSERT INTO invoice VALUES(inid, oid, description, dueDate, amount,custName , method, status) 
 
-        records += [stmt]
+        records += [stmt] # save statements to list
         inids += [inid]
 
     return inids, records
-
 
 # create the request statements
 inids, invoice_insertion = create_invoice(customers, oids)
@@ -401,17 +364,11 @@ with open("8_invoice_insertions.sql", "w") as file:
     file.writelines(invoice_insertion)
     file.close()
 
-
 ###############################################################################
 
-### 3.9 startTable table 
-def create_startTable(rids, customers, mates):
-    """
-    A Customer starts a Request to a Mate
-    Will create records of the form: 
-        INSERT INTO startTable VALUES(rid, mateName, custName, startDate)
-    """
-    
+### 3.8 startTable table 
+def create_startTable(rids, customers, mates): #arguments are a list of request id, a list of customer, and a list of mate
+
     size = len(rids)
     records = []  # store the records
 
@@ -419,8 +376,7 @@ def create_startTable(rids, customers, mates):
 #    matesNames = np.random.choice(mates, size=size, replace=False) 
     customerNames = customers 
     matesNames = mates 
-    
-    
+      
     # Set up time generation objects
     fake_time = Faker()
     start_date0 = datetime.date(year=2018, month=1, day=1)  # suppose our business started in 2018
@@ -431,10 +387,11 @@ def create_startTable(rids, customers, mates):
         start_date = start_dates[i]
         stmt = "INSERT INTO startTable VALUES({},'{}','{}','{}');\n".format(
             rids[i], matesNames[i], customerNames[i],str(start_date))
-        records += [stmt]
+        #SQL: INSERT INTO startTable VALUES(rid, mateName, custName, startDate)
+
+        records += [stmt] # save statements to list
         
     return records
-
 
 # create the request statements
 # the "r" lists came from the script to create the requests
@@ -445,14 +402,11 @@ with open("9_startTable_insertions.sql", "w") as file:
     file.writelines(startTable_insertion)
     file.close()
     
-    
-
 ###############################################################################
 
-### 3.10 Activity table
+### 3.9 Activity table
 
-def create_activity(managers):
-    #aid, description, mngName
+def create_activity(managers): # argument is a list of manager
 
     records = []  # store the records
     
@@ -465,7 +419,9 @@ def create_activity(managers):
     for i in range(size):
         aid = i+1
         stmt = "INSERT INTO activity VALUES({},'{}','{}');\n".format(aid, descriptions[i],managersNames[i])
-        records += [stmt]
+        #SQL: INSERT INTO activity VALUES(aid, descriptions, managerName)
+
+        records += [stmt] # save statements to list
         aids += [aid] 
         
     return aids, records
@@ -478,19 +434,15 @@ with open("10_activity_insertions.sql", "w") as file:
     file.writelines(activity_insertion)
     file.close()
 
-
-## ** WRITE export_sql code in here ** ##
-
-    
 ###############################################################################
 
-### 3.11 modify table
+### 3.10 modify table
     
-def create_modify(managers):
+def create_modify(managers): # argument is a list of manager
     
     size = len(managers)
     records = []  # store the records
-    oids = random.sample(range(1, size + 1), size)
+    oids = random.sample(range(1, size + 1), size) #randomly choose order id; all are valid note that #order is larger than #manager
     manager = random.sample(managers, size)
 
     # create time
@@ -503,10 +455,11 @@ def create_modify(managers):
     for i in range(size):
         stmt = "INSERT INTO modification VALUES('{}',{},'{}');\n".format(
             manager[i], oids[i], modTimes[i])
-        records += [stmt]
+        #SQL: INSERT INTO modification VALUES(managerName, oid, modTimes)
+
+        records += [stmt] # save statements to list
 
     return records
-
 
 # create the request statements
 modify_insertion = create_modify(managers)
@@ -518,12 +471,9 @@ with open("11_modify_insertions.sql", "w") as file:
     
 ###############################################################################
 
-### 3.12 generate table
+### 3.11 generate table
     
-def create_generate(oids, rids):
-    """ 
-    Assume oids and rids have same length (check later)
-    """
+def create_generate(oids, rids): # arguments are a list of oid and a list of request id 
     
     size = len(oids) # 
     records = []  # store the records
@@ -533,12 +483,12 @@ def create_generate(oids, rids):
     for i in range(size):
         oid = oids[i]
         rid = rids[i]
-        stmt = "INSERT INTO generate VALUES({},{});\n".format(
-            rid, oid)
-        records += [stmt]
+        stmt = "INSERT INTO generate VALUES({},{});\n".format(rid, oid)
+        #SQL: INSERT INTO generate VALUES(rid, oid)
+
+        records += [stmt] # save statements to list
 
     return records
-
 
 # create the generate statements
 generate_insertion = create_generate(oids,rids)
@@ -548,13 +498,12 @@ with open("12_generate_insertions.sql", "w") as file:
     file.writelines(generate_insertion)
     file.close()
 
-    
 ###############################################################################
 
 
-### 3.13 Schedule table
+### 3.12 Schedule table
 
-def create_schedule(oids, aids):
+def create_schedule(oids, aids): #  arguments are a list of order id and a list of activity id
     
     records = []  # store the records
     size = len(oids) # 
@@ -562,8 +511,10 @@ def create_schedule(oids, aids):
     
     # create size number of records
     for i in range(size):
-        stmt = "INSERT INTO schedule VALUES({},{});\n".format( aids[i], oids[i])
-        records += [stmt]
+        stmt = "INSERT INTO schedule VALUES({},{});\n".format(aids[i], oids[i])
+        #SQL: INSERT INTO schedule VALUES(aid, oid)
+
+        records += [stmt] # save statements to list
 
     return records
 
