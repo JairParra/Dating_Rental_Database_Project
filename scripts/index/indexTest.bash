22ca786e3ab4ece8e25419ce43b603fd514af9e0
qqqqyyy <44212024+qqqqyyy@users.noreply.github.com>
2020-04-08 13:26:39 -0400

    Add files via upload

--- scripts/index/indexTest.bash
+++ scripts/index/indexTest.bash
@@ -2,24 +2,24 @@
 echo "Without Index" > indexTest.log
 echo "indexTest1_noIndex.sql" >> indexTest.log
 echo "------------------------------------------" >>indexTest.log
-cat indexTest1_noIndex.sql >>result_creation.txt
+cat indexTest1_noIndex.sql >>indexTest.log
 psql cs421< indexTest1_noIndex.sql  >>indexTest.log
 
 echo "With Index" >> indexTest.log
 echo "indexTest1_index.sql" >> indexTest.log
 echo "------------------------------------------" >>indexTest.log
-cat indexTest1_index.sql >> result_creation.txt
+cat indexTest1_index.sql >> indexTest.log
 psql cs421< indexTest1_index.sql >> indexTest.log
 
 echo "Without Index" >> indexTest.log
 echo "indexTest2_noIndex.sql" >> indexTest.log
 echo "------------------------------------------" >>indexTest.log
-cat indexTest2_noIndex.sql >>result_creation.txt
+cat indexTest2_noIndex.sql >>indexTest.log
 psql cs421< indexTest2_noIndex.sql  >>indexTest.log
 
 echo "With Index" >> indexTest.log
 echo "indexTest2_index.sql" >> indexTest.log
 echo "------------------------------------------" >>indexTest.log
-cat indexTest2_index.sql >> result_creation.txt
+cat indexTest2_index.sql >> indexTest.log
 psql cs421< indexTest2_index.sql >> indexTest.log
 
